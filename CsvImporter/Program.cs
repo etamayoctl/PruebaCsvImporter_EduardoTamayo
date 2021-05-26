e051ae84b0b661ca5f9e7cd7eb007a6727cec361
Eduardo Tamayo <70561732+etamayoctl@users.noreply.github.com>
2021-05-26 20:13:26 -0300

    Add files via upload

---
+++ CsvImporter/Program.cs
@@ -0,0 +1,40 @@
+﻿using Azure.Storage.Blobs;
+using CsvImporter.Model;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace CsvImporter
+{
+    class Program
+    {
+
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            Console.Title = "Acme Corporation";
+            BlobClient blobClient = new BlobClient(new Uri(Util.GetUrl()), null);
+            if (await blobClient.ExistsAsync())
+            {
+                var response = await blobClient.DownloadAsync();
+                using (var streamReader = new StreamReader(response.Value.Content))
+                {
+                    if (ConnectToBD.ConectBD())
+                    {
+                        if (!streamReader.EndOfStream)
+                        {
+                            ConnectToBD.CreateTable(await streamReader.ReadLineAsync());
+                        }
+                        while (!streamReader.EndOfStream)
+                        {
+                            ConnectToBD.CastCsvLineToObject(await streamReader.ReadLineAsync());
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+}
---
+++ CsvImporter/Util.cs
@@ -0,0 +1,27 @@
+﻿using CsvImporter.Model;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsvImporter
+{
+    class Util
+    {
+        public static string GetUrl()
+        {
+            var ruta = System.IO.Path.GetFullPath(@"..\..\..\");
+            var file = "Configs\\AzureCsv.json";
+            Url u = new Url();
+            using (StreamReader r = new StreamReader(ruta + file))
+            {
+                string json = r.ReadToEnd();
+                u = JsonConvert.DeserializeObject<Url>(json);
+            }
+            return u.UrlCsv;
+        }
+    }
+}
